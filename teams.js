b932e260984db5bcd07ff08d65ac039c19bc8c09
Andrés <usuario616@gmail.com>
2021-01-08 13:54:07 +0100

    Añadido método opcional para importar desde CountryAPI.

--- teams.js
+++ teams.js
@@ -29,4 +29,22 @@ export async function getCountriesFromGithub() {
     
 }
 
+export async function getCountriesFromCountryAPI() {
+    try{
+
+        const url = 'http://countryapi.gear.host/v1/Country/getCountries';
+        const response = await axios.get(url);
+        //console.log(response.data.Response);
+        const paises=response.data.Response.map( pais => pais.Name );
+        return paises;
+
+    }catch{
+        
+        //Si el repo está caído o lo que sea.
+        console.warn("Aviso: No se pudo obtener los países desde el repositorio.\nSe recogen desde una lista local");
+        return paisesB;
+        
+    }
+}
+
    
